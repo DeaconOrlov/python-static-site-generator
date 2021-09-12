66190cc327f485727627d6a88050b83d8fb454ab
DeaconOrlov <deaconorlov@gmail.com>
2021-09-12 11:46:50 -0400

    add copy method and ResourceParser class to parsers.py

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -1,5 +1,8 @@
+import shutil
+
 from typing import List
 from pathlib import Path
+
 
 class Parser:
     extension: List[str] = []
@@ -18,3 +21,13 @@ class Parser:
         full_path = dest / path.with_suffix(ext).name
         with open(full_path, "w") as file:
             file.write(content)
+
+    def copy(self, path, source, dest):
+        shutil.copy2(path, dest / path.relative_to(source))
+
+
+class ResourceParser(Parser):
+    extensions = [".jpg", ".png", ".gif", ".css", ".html"]
+
+    def parse(self, path, source, dest):
+        self.copy(path, source, dest)
